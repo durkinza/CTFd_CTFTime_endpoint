278a20d9d6e08feb8b2d40c755bc23288eccc05c
Zane Durkin <zanedurkin@gmail.com>
2019-09-09 13:01:53 -0700

    adding taskStats to each team

--- routes.py
+++ routes.py
@@ -1,18 +1,19 @@
 from flask_restplus import Namespace, Resource
 from flask import session, jsonify
 
-from CTFd.models import Challenges
+from CTFd.models import Solves, Challenges
 from CTFd.cache import cache, make_cache_key
 from CTFd.utils.scores import get_standings
 from CTFd.utils import get_config
-from CTFd.utils.modes import get_mode_as_word, TEAMS_MODE
-from CTFd.utils.dates import isoformat
+from CTFd.utils.modes import get_model, TEAMS_MODE
+from CTFd.utils.dates import unix_time
 from CTFd.utils.decorators import (
     during_ctf_time_only
 )
 from CTFd.utils.decorators.visibility import (
     check_challenge_visibility,
     check_score_visibility,
+	check_account_visibility
 )
 from CTFd.utils.config.visibility import (
     scores_visible,
@@ -26,35 +27,51 @@ ctftime_namespace = Namespace('ctftime', description="Endpoint to retrieve score
 
 @ctftime_namespace.route('')
 class ScoreboardList(Resource):
-    @check_challenge_visibility
-    @during_ctf_time_only
-    @check_score_visibility
-    @cache.cached(timeout=60, key_prefix=make_cache_key)
-    def get(self):
-        response = {
-            'tasks': [],
-            'standings': []
-        }
+	@check_challenge_visibility
+	@during_ctf_time_only
+	@check_score_visibility
+	@check_account_visibility
+	@cache.cached(timeout=60, key_prefix=make_cache_key)
+	def get(self):
+		response = {
+			'tasks': [],
+			'standings': []
+		}
 
-        mode = get_config("user_mode")
-        account_type = get_mode_as_word()
+		mode = get_config("user_mode")
+		freeze = get_config("freeze")
 
-        challenges = Challenges.query.filter(
-            and_(Challenges.state != 'hidden', Challenges.state != 'locked')
-        ).order_by(Challenges.value).all()
+		challenges = Challenges.query.filter(
+			and_(Challenges.state != 'hidden', Challenges.state != 'locked')
+		).order_by(Challenges.value).all()
 
-        for i, x in enumerate(challenges):
-            response['tasks'].append(x.name+" "+str(x.value))
+		challenges_ids={}
+		for i, x in enumerate(challenges):
+			response['tasks'].append(x.name+" "+str(x.value))
+			challenges_ids[x.id]=x.name
 
-        if mode == TEAMS_MODE:
-            standings = get_standings()
-            team_ids = [team.account_id for team in standings]
+		if mode == TEAMS_MODE:
+			standings = get_standings()
+			team_ids = [team.account_id for team in standings]
 
-        for i, team in enumerate(team_ids):
-            response['standings'].append({
-                'pos': i+1,
-                'team': standings[i].name,
-                'score': float(standings[i].score)
-            })
+		solves = Solves.query.filter(Solves.account_id.in_(team_ids))
+		if freeze:
+			solves = solves.filter(Solves.date < unix_time_to_utc(freeze))
 
-        return response
+		for i, team in enumerate(team_ids):
+			team_standing = {
+				'pos': i+1,
+				'team': standings[i].name,
+				'score': float(standings[i].score),
+				'taskStats': {}
+			}
+			for solve in solves:
+				if solve.account_id == team:
+					chall_name = challenges_ids[solve.challenge_id]
+					print(solve.date)
+					team_standing["taskStats"][chall_name] ={
+						"points": solve.challenge.value,
+						"time": unix_time(solve.date),
+					}
+			response['standings'].append(team_standing)
+		return response
